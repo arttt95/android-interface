f3432df4757e54a2de7d5590fb50bdeead82b6a5
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-03 20:36:14 -0300

    03DEZ24 - Aula 73 AS - Eventos setOnClickListener + R.java class.

--- app/src/main/java/com/arttt95/componentesinterface/CaixaActivity.java
+++ app/src/main/java/com/arttt95/componentesinterface/CaixaActivity.java
@@ -1,6 +1,8 @@
 package com.arttt95.componentesinterface;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,23 @@ public class CaixaActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        /*  Usando uma função anônima para instânciar uma View
+        Button botao = findViewById(R.id.botao_salvar);
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                System.out.println("Botão foi clicado!");
+
+            }
+        });*/
+
+        // Utilizando uma função lambda
+        Button botao = findViewById(R.id.botao_salvar);
+        botao.setOnClickListener(v -> System.out.println("Botão foi clicado!"));
+
     }
+
+
 }
